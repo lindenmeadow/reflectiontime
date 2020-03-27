2d0a57b041bfdaddd333d49739c1b07761ba493e
Evan B <konros@yahoo.com>
2020-03-27 15:13:41 -0400

    Add files via upload

---
+++ app/controllers/reflections_controller.rb
@@ -0,0 +1,91 @@
+class ReflectionsController < ApplicationController
+  get '/reflections' do
+    if logged_in?
+      @reflections = Reflection.all
+      erb :'reflections/index'
+    else
+      erb :'users/login', locals: {message: "You aren't logged in yet."}
+    end
+  end
+
+  get '/reflections/new' do
+    if logged_in?
+      erb :'reflections/new'
+    else
+      erb :'users/login', locals: {message: "You need to login first."}
+    end
+  end
+
+  post '/reflections' do
+    if logged_in?
+        @reflection = Reflection.create(
+          student_name: params[:student_name],
+          block: params[:block],
+          teacher_name: params[:teacher_name],
+          project_name: params[:project_name],
+          project_type: params[:project_type],
+          conduct: params[:conduct],
+          leadership: params[:leadership],
+          work: params[:work],
+          creativity: params[:creativity],
+          collaboration: params[:collaboration],
+          thinking: params[:thinking],
+          communication: params[:communication],
+          comments: params[:comments]
+          )
+        @reflection.user_id = current_user.id
+        @reflection.save
+        redirect "/reflections/#{@reflection.id}"
+      else
+        erb :'users/login', locals: {message: "You need to login first."}
+      end
+  end
+
+  get '/reflections/:id' do
+    if logged_in?
+      @reflection = Reflection.find_by_id(params[:id])
+      erb :'reflections/show'
+    else
+      erb :'users/login', locals: {message: "You need to login first."}
+    end
+  end
+
+  get '/reflections/:id/edit' do
+    if logged_in?
+      @reflection = Reflection.find_by_id(params[:id])
+      if @reflection && @reflection.user == current_user
+        erb :'reflections/edit'
+      else
+        erb :'reflections/index', locals: {message: "You can't edit someone else's reflection."}
+      end
+    else
+      erb :'users/login', locals: {message: "You need to login first."}
+    end
+  end
+
+  patch '/reflections/:id' do
+    @reflection = Reflection.find_by_id(params[:id])
+    if logged_in? && @reflection.user == current_user
+      @reflection.update(
+        project_name: params[:project_name],
+        student_name: params[:student_name],
+        block: params[:block],
+        teacher_name: params[:teacher_name])
+        redirect "/reflections/#{@reflection.id}"
+    else
+        redirect '/login'
+    end
+  end
+
+  delete '/reflections/:id/delete' do
+    @reflection = Reflection.find_by_id(params[:id])
+    if logged_in?
+      if @reflection && @reflection.user == current_user
+        @reflection.destroy
+      end
+      erb :'reflections/index'
+    else
+      erb :'users/login', locals: {message: "You need to login first."}
+    end
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+
+  get '/users/:id' do
+    if logged_in?
+      erb :'/users/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect to '/reflections'
+    else
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == '' || params[:password] == ''
+      redirect '/signup'
+    elsif User.find_by(username: username) != nil
+        #message that username already exists
+    else
+      @user = User.create(username: params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect to '/reflections'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      #message that user is already logged in
+      redirect to '/reflections'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect '/reflections'
+   else
+     #message that username and/or password is incorrect
+     redirect '/login'
+   end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+end
