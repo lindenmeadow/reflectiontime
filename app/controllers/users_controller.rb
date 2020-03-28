f5c3eedc7f55e2ea33a163b8cfac60ec19e50781
Evan B <konros@yahoo.com>
2020-03-27 23:08:56 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(:username => params[:username])
+    user = User.find_by(:name => params[:name])
     if user && user.authenticate(params[:password])
      session[:user_id] = user.id
      redirect '/reflections'
