f8b4ec5cb1cf0828061377636a65852c12e81d21
Evan B <konros@yahoo.com>
2020-04-05 18:25:55 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :reflections
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :name, length: 4..10
+  validates :password, presence: true
+  validates :password, length: 4..12
 end
